29464e3076e62c42060e6de31f5d2da142f1e872
Pedro Lucas Carneiro <pedro.lucas.root@gmail.com>
2022-03-30 22:43:06 -0300

    refactor(atvd) 02 added

---
+++ 02/script_1.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../source.png')
+
+cv2.imshow('original', img)
+
+(height, width) = img.shape[0:2]
+
+low_width = int(width/3)
+low_height = int(height/3)
+low_quality_img = np.zeros((low_height, low_width, 3), np.uint8)
+
+
+def nine2one(matrix):
+    return matrix[1, 1]
+
+
+for y in range(low_width):
+    start_y = y * 3
+    for x in range(low_height):
+        start_x = x * 3
+        target_3x3 = img[start_x:start_x+3, start_y: start_y+3]
+        low_quality_img[x, y] = nine2one(target_3x3)
+
+
+cv2.imshow('low quality', low_quality_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ 02/script_2.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../source.png')
+
+cv2.imshow('original', img)
+
+(height, width) = img.shape[0:2]
+low_quality_img = np.zeros(img.shape, np.uint8)
+
+
+def nine2one(matrix):
+    return cv2.mean(matrix)[:3]
+
+
+for y in range(width):
+    for x in range(height):
+        target_3x3 = img[x-1:x+2, y-1: y+2]
+        low_quality_img[x, y] = nine2one(target_3x3)
+
+
+cv2.imshow('low quality', low_quality_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
